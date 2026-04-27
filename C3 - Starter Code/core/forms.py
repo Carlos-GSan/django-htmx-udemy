98b1c9c3f151d38238bb20e9a07914785124a3be
Carlos GSan <gscarlos.enri@gmail.com>
2026-04-27 17:56:00 +0000

    30 video

--- C3 - Starter Code/core/forms.py
+++ C3 - Starter Code/core/forms.py
@@ -1,1 +1,29 @@
 from django import forms
+from core.models import Book
+
+class BookForm(forms.Form):
+    name = forms.CharField(
+        max_length=128,
+        widget=forms.TextInput(attrs={'class': 'input input-primary'})
+    )
+    genre = forms.ChoiceField(
+        choices=Book.GenreChoices.choices,
+        widget=forms.Select(attrs={'class': 'select select-primary'})
+    )
+    
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+    
+    def clean(self):
+        # check the database to see if the user already has the book in their list
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        genre = cleaned_data.get('genre')
+        
+        if name and genre and self.user:
+            # check if the book exists
+            if self.user.books.filter(name=name, genre=genre).exists():
+                raise forms.ValidationError(f"You already have '{name}' in your book list!")
+            
+        return cleaned_data
--- C3 - Starter Code/core/views.py
+++ C3 - Starter Code/core/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from core.forms import BookForm
+from core.models import Book
 
 # Create your views here.
+@login_required
 def index(request):
-    context = {}
+    if request.method == 'POST':
+        form =  BookForm(request.POST, user=request.user)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            genre = form.cleaned_data['genre']
+            book, _ = Book.objects.get_or_create(name=name, genre=genre)
+            if not request.user.books.filter(id=book.id).exists():
+                request.user.books.add(book)
+                response =  render(request, 'partials/book-row.html', {'book': book})
+                return response
+        else:
+            context = {'form': form}
+            response =  render(request, 'partials/book-form.html',context)
+            response['HX-Retarget'] = '#book-form'
+            response['HX-Reswap'] = 'outerHTML'
+            return response
+    books = request.user.books.all()
+    context = {'books': books, 'form': BookForm()}
     return render(request, 'index.html', context)
