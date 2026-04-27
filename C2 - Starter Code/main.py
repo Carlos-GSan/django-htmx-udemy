929451aa79f3458ff010b5997c6af5fd11f08341
Carlos GS <gscarlos.enri@gmail.comt push>
2026-04-27 14:52:33 -0600

    adding new

---
+++ C2 - Starter Code/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from c2-starter-code!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ C3 - Starter Code/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from c3-starter-code!")
+
+
+if __name__ == "__main__":
+    main()
